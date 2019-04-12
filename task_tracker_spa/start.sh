be1ad3688383e4ab393a008403e316975b22d5d2
brennas22 <brenna.sorkin@gmail.com>
2019-04-12 11:37:03 -0400

    update start script

--- task_tracker_spa/start.sh
+++ task_tracker_spa/start.sh
@@ -5,8 +5,8 @@ export PORT=4794
 
 echo "Stopping old version."
 
-_build/prod/rel/task_tracker/bin/task_tracker stop || true
+_build/prod/rel/task_tracker_spa/bin/task_tracker_spa stop || true
 
 echo "Starting Task Tracker."
 
-_build/prod/rel/task_tracker/bin/task_tracker foreground
+_build/prod/rel/task_tracker_spa/bin/task_tracker_spa foreground
